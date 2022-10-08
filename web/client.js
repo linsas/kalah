c36c221fb8254ab44434a27a29277e795dcd5182
linsas <linsas0107@gmail.com>
2022-10-08 14:55:32 +0300

    Improve board styles

--- web/client.js
+++ web/client.js
@@ -5,53 +5,40 @@ const socket = io({
 })
 
 socket.on('connect', () => {
-	document.querySelector('div#board').classList.add('connected')
+	document.querySelector('div#board').classList.remove('offline')
 })
 socket.on('disconnect', () => {
-	document.querySelector('div#board').classList.remove('connected')
+	document.querySelector('div#board').classList.remove('southTurn', 'northTurn')
+	document.querySelector('div#board').classList.add('offline')
 })
 
-const getVesselId = vessel => (vessel > numBins ? 'north' : 'south') + (vessel % (numBins + 1) === numBins ? 'Store' : ('Bin' + (vessel % (numBins + 1) + 1)))
-
 socket.on('update', (gamestate) => {
-	const vessels = document.querySelectorAll('div#board > div')
-	vessels.forEach((node) => {
-		node.classList.remove('turn')
-		if (node.classList.contains(gamestate.isNorthTurn ? 'north' : 'south')) node.classList.add('turn')
-	})
+	document.querySelector('div#board').classList.remove('southTurn', 'northTurn')
+	document.querySelector('div#board').classList.add(gamestate.isNorthTurn ? 'northTurn' : 'southTurn')
 
-	for (let index = 0; index < gamestate.board.length; index++) {
-		const stones = gamestate.board[index]
-		const element = document.querySelector('div#' + getVesselId(index))
-		element.innerText = stones
+	const labelElements = document.querySelectorAll('.vessel > label')
+	for (let vessel = 0; vessel < gamestate.board.length; vessel++) {
+		labelElements[vessel].textContent = gamestate.board[vessel]
 	}
 })
 
+const handleClickVessel = (vessel) => {
+	socket.emit('play', vessel)
+}
+
 const handleConnect = () => {
-	if (socket.connected) return
 	socket.connect()
 }
 
 const handleDisconnect = () => {
-	if (socket.disconnected) return
 	socket.disconnect()
 }
+
 
 document.querySelector('button#connect').addEventListener('click', handleConnect)
 document.querySelector('button#disconnect').addEventListener('click', handleDisconnect)
 
-
-const handleClickVessel = (vessel) => {
-	if (socket.connected) {
-		socket.emit('play', vessel)
-	}
+const vessels = document.querySelectorAll('#board > .vessel')
+for (let index = 0; index < vessels.length; index++) {
+	vessels[index].addEventListener('click', () => handleClickVessel(index))
 }
-
-for (let index = 0; index < numBins; index++) {
-	const southBin = document.querySelector('div#southBin' + (index + 1))
-	const northBin = document.querySelector('div#northBin' + (index + 1))
-	southBin.addEventListener('click', () => handleClickVessel(index))
-	northBin.addEventListener('click', () => handleClickVessel(index + numBins + 1))
-}
-document.querySelector('div#southStore').addEventListener('click', () => handleClickVessel(numBins))
-document.querySelector('div#northStore').addEventListener('click', () => handleClickVessel(numBins * 2 + 1))
